8b8134813498b82034a8f3573fbef49048e637df
anchel.silva <anchel.varela@gmail.com>
2025-03-26 10:19:40 -0300

    first commit

---
+++ src/main/java/com/esig/quarkrh/QuarkrhApplication.java
@@ -0,0 +1,13 @@
+package com.esig.quarkrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarkrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuarkrhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esig/quarkrh/QuarkrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esig.quarkrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarkrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
