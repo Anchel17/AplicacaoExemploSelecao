587dd9a911cd7725d94aa7765e99ae9ce06ed922
anchel.silva <anchel.varela@gmail.com>
2025-03-28 09:50:55 -0300

    Adição de mensagem de validação para formulário de login

--- quarkrh-web/src/app/components/sign-up/sign-up.component.ts
+++ quarkrh-web/src/app/components/sign-up/sign-up.component.ts
@@ -2,23 +2,22 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { SignUpService } from './sign-up.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   imports: [MatTabsModule, MatFormFieldModule, MatInputModule, FormsModule,
-    MatButtonModule, ReactiveFormsModule, MatSelectModule, HttpClientModule],
+    MatButtonModule, ReactiveFormsModule, MatSelectModule, HttpClientModule, CommonModule],
   providers: [SignUpService],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  public usuario: string;
-  public senha: string;
   public role: string;
 
   public loginFormGroup: FormGroup;
@@ -30,11 +29,38 @@ export class SignUpComponent {
     this.inicializarForms();
   }
 
+  get login(){
+    return this.loginFormGroup.get('login')!;
+  }
+
+  get senha(){
+    return this.loginFormGroup.get('senha')!;
+  }
+
+  get cadastroFormLogin(){
+    return this.cadastroFormGroup.get('login')!;
+  }
+
+  get cadastroFormSenha(){
+    return this.cadastroFormGroup.get('senha')!;
+  }
+
+  get cadastroFormRole(){
+    return this.cadastroFormGroup.get('role')!;
+  }
+
   onSubmitLoginForm(){
+    if(this.loginFormGroup.invalid){
+      return;
+    }
     this.signUpService.login({login: this.loginFormGroup.value.login, password: this.loginFormGroup.value.senha});
   }
 
   onSubmitCadastroForm(){
+    if(this.cadastroFormGroup.invalid){
+      return;
+    }
+
     this.signUpService.cadastrar({
       login: this.cadastroFormGroup.value.login,
       password: this.cadastroFormGroup.value.senha,
